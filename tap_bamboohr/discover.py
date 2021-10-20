49e196e9f000e03f16f8f07c0562f75ed1531801
Leon Smith <leon12smith@gmail.com>
2021-10-20 11:23:23 +0200

    Working discovery mode for tap

--- tap_bamboohr/discover.py
+++ tap_bamboohr/discover.py
@@ -6,7 +6,7 @@ from .streams import STREAMS
 
 from singer.schema import Schema
 from singer import utils, metadata
-from singer.catalog import Catalog, CatalogEntry
+from singer.catalog import Catalog
 
 # from singer.metadata import get_standard_metadata
 
@@ -49,51 +49,20 @@ def _get_schemas_meta():
 # Tap discovery mode to retrieve schema and metadata info
 def discover():
     schemas, schemas_metadata = _get_schemas_meta()
+    streams = []
 
-
-    # try schema_metadata.key_properties print
-
-# def _load_schemas():
-#     """ Load schemas from schemas folder """
-#     schemas = {}
-
-#     for filename in os.listdir(get_abs_path('schemas')):
-#     path = get_abs_path('schemas') + '/' + filename
-#     file_raw = filename.replace('.json', '')
-#     with open(path) as file:
-#         schemas[file_raw] = Schema.from_dict(json.load(file))
-
-
-# def discover():
-#     raw_schemas = load_schemas()
-#     print("doing discover")
-#     streams = []
-#     for stream_id, schema in raw_schemas.items():
-#         # TODO: populate any metadata and stream's key properties here..
-#         # Need to add something here to get metadata - currently not working
-#         stream_metadata = [{'selected': True}]
-#         # stream_metadata = get_standard_metadata(schema="",
-#         #                                         schema_name=stream_id,
-#         #                                         key_properties=["id"],
-#         #                                         valid_replication_keys=None,
-#         #                                         replication_method=None)
+    # Get each schema and schema_metadata to create Catalog file
+    for schema_name, schema in schemas.items():
+        schema_meta = schemas_metadata[schema_name]
         
-#         # Assign the stream key property - primary key
-#         key_properties = ['id']
-#         streams.append(
-#             CatalogEntry(
-#                 tap_stream_id=stream_id,
-#                 stream=stream_id,
-#                 schema=schema,
-#                 key_properties=key_properties,
-#                 metadata=stream_metadata,
-#                 replication_key=None,
-#                 is_view=None,
-#                 database=None,
-#                 table=None,
-#                 row_count=None,
-#                 stream_alias=None,
-#                 replication_method=None,
-#             )
-#         )
-#     return Catalog(streams)
+        # Catalog format
+        catalog_entry = {
+            "stream": schema_name,
+            "tap_stream_id": schema_name,
+            "schema": schema,
+            "metadata":schema_meta
+        }
+
+        streams.append(catalog_entry)
+
+    return Catalog.from_dict({"streams": streams})
